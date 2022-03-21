60a4c6ca0103a3d13c13e699b7fd08a9e4a7768a
Andre12Lopes <andre.flopes@outlook.pt>
2022-03-21 12:09:42 +0000

    Added transfer between 16 acc

--- test/bank/bank.c
+++ test/bank/bank.c
@@ -39,7 +39,9 @@ int main()
     struct stm_tx tx;
     int rand, tid;
     int ra, rb, rc, rd, re, rf, rg, rh;
+    int ri, rj, rl, rm, rn, ro, rp, rq
     unsigned int a, b, c, d, e, f, g, h;
+    unsigned int i, j, l, m, n, o, p, q;
     uint64_t s;
     char buffer[BUFFER_SIZE];
     int idx = 0;
@@ -76,6 +78,16 @@ int main()
         rf = RAND_R_FNC(s) % N_ACCOUNTS;
         rg = RAND_R_FNC(s) % N_ACCOUNTS;
         rh = RAND_R_FNC(s) % N_ACCOUNTS;
+
+        ri = RAND_R_FNC(s) % N_ACCOUNTS; 
+        rj = RAND_R_FNC(s) % N_ACCOUNTS; 
+        rl = RAND_R_FNC(s) % N_ACCOUNTS; 
+        rm = RAND_R_FNC(s) % N_ACCOUNTS; 
+        rn = RAND_R_FNC(s) % N_ACCOUNTS; 
+        ro = RAND_R_FNC(s) % N_ACCOUNTS; 
+        rp = RAND_R_FNC(s) % N_ACCOUNTS; 
+        rq = RAND_R_FNC(s) % N_ACCOUNTS;
+
 #ifdef RO_TX
         rand = (RAND_R_FNC(s) % 100) + 1;
 #endif
@@ -113,20 +125,53 @@ int main()
         STORE(&tx, &bank[rd], d, t_aborts);
 
         e = LOAD(&tx, &bank[re], t_aborts, tid);
-        e += TRANSFER;
+        e -= TRANSFER;
         STORE(&tx, &bank[re], e, t_aborts);
 
         f = LOAD(&tx, &bank[rf], t_aborts, tid);
-        f += TRANSFER;
+        f -= TRANSFER;
         STORE(&tx, &bank[rf], f, t_aborts);
 
         g = LOAD(&tx, &bank[rg], t_aborts, tid);
-        g += TRANSFER;
+        g -= TRANSFER;
         STORE(&tx, &bank[rg], g, t_aborts);
 
         h = LOAD(&tx, &bank[rh], t_aborts, tid);
-        h += TRANSFER;
+        h -= TRANSFER;
         STORE(&tx, &bank[rh], h, t_aborts);
+
+
+        i = LOAD(&tx, &bank[ri], t_aborts, tid);
+        i += TRANSFER;
+        STORE(&tx, &bank[ri], i, t_aborts);
+
+        j = LOAD(&tx, &bank[rj], t_aborts, tid);
+        j += TRANSFER;
+        STORE(&tx, &bank[rj], j, t_aborts);
+
+        l = LOAD(&tx, &bank[rl], t_aborts, tid);
+        l += TRANSFER;
+        STORE(&tx, &bank[rl], l, t_aborts);
+
+        m = LOAD(&tx, &bank[rm], t_aborts, tid);
+        m += TRANSFER;
+        STORE(&tx, &bank[rm], m, t_aborts);
+
+        n = LOAD(&tx, &bank[rn], t_aborts, tid);
+        n += TRANSFER;
+        STORE(&tx, &bank[rn], n, t_aborts);
+
+        o = LOAD(&tx, &bank[ro], t_aborts, tid);
+        o += TRANSFER;
+        STORE(&tx, &bank[ro], o, t_aborts);
+
+        p = LOAD(&tx, &bank[rp], t_aborts, tid);
+        p += TRANSFER;
+        STORE(&tx, &bank[rp], p, t_aborts);
+
+        q = LOAD(&tx, &bank[rq], t_aborts, tid);
+        q += TRANSFER;
+        STORE(&tx, &bank[rq], q, t_aborts);
 
         COMMIT(&tx, t_aborts);
 #endif
