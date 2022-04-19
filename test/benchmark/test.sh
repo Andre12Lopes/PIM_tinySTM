1ad1fb7dba0d7be89494451e3401047926be5d82
Andre12Lopes <andre.flopes@outlook.pt>
2022-04-19 21:45:18 +0100

    Added backoff

--- test/benchmark/test.sh
+++ test/benchmark/test.sh
@@ -3,13 +3,13 @@ echo -e "N_THREADS\tN_TRANSACTIONS\tTIME\tN_ABORTS\tPROCESS_TIME\tCOMMIT_TIME\tW
 
 make
 
-for (( i = 1; i < 25; i++ )); do
+for (( i = 1; i < 21; i++ )); do
 	cd ../bank
 	make clean
 	make NR_TASKLETS=$i
 	cd ../benchmark
 	
-	for (( j = 0; j < 1; j++ )); do
+	for (( j = 0; j < 30; j++ )); do
 		./launch >> results.txt
 	done
 done
